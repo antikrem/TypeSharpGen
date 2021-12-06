a8acfa88160d776976a41aeb0b671a7c69509738
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-12-07 01:13:22 +1000

    Added float and double to build ins

--- TypeSharpGenLauncher/Core/Constructor/TypeScriptBuiltInTypes.cs
+++ TypeSharpGenLauncher/Core/Constructor/TypeScriptBuiltInTypes.cs
@@ -17,11 +17,18 @@ namespace TypeSharpGenLauncher.Core.Constructor
 
     public class TypeScriptBuiltInTypes : ITypeScriptBuiltInTypes
     {
+        private readonly ITypeReducer _typeReducer;
+
+        public TypeScriptBuiltInTypes(ITypeReducer typeReducer)
+        {
+            _typeReducer = typeReducer;
+        }
+
         public ISet<Type> BuiltInTypes => new HashSet<Type>(BuiltInTypeSymbols.Keys);
 
         public IDictionary<Type, string> BuiltInTypeSymbols
             => InnerBuiltInTypeSymbols()
-                .SelectMany(builtIn => DerivedTypes(builtIn.Type, builtIn.Name))
+                .SelectMany(builtIn => _typeReducer.ComposedTypes(builtIn.Type, builtIn.Name))
                 .Compose();
 
         public static IEnumerable<(Type Type, string Name)> InnerBuiltInTypeSymbols()
@@ -31,20 +38,9 @@ namespace TypeSharpGenLauncher.Core.Constructor
             yield return (typeof(bool), "boolean");
             yield return (typeof(int), "number");
             yield return (typeof(long), "number");
+            yield return (typeof(float), "number");
+            yield return (typeof(double), "number");
             yield return (typeof(object), "any");
-        }
-
-        private static IEnumerable<(Type Type, string Name)> DerivedTypes(Type type, string name)
-        {
-            yield return (type, name);
-            yield return (type.MakeArrayType(), $"{name}[]");
-            if (type != typeof(void))
-            {
-                yield return (typeof(IEnumerable<>).MakeGenericType(type), $"{name}[]");
-                yield return (typeof(IReadOnlyList<>).MakeGenericType(type), $"{name}[]");
-                yield return (typeof(List<>).MakeGenericType(type), $"{name}[]");
-            }
-
         }
     }
 }
