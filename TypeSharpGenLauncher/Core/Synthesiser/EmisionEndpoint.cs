c794f23ea7a689adfcd8c88bb0fb3f0621db3745
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-12-07 01:11:49 +1000

    Added emmision endpoint for writing file

--- TypeSharpGenLauncher/Core/Synthesiser/EmisionEndpoint.cs
+++ TypeSharpGenLauncher/Core/Synthesiser/EmisionEndpoint.cs
@@ -1,9 +1,13 @@
-﻿using EphemeralEx.Injection;
-using System;
+﻿using System;
+using System.Text;
+
+using EphemeralEx.Injection;
+using EphemeralEx.FileSystem;
+
 
 namespace TypeSharpGenLauncher.Core.Synthesiser
 {
-    [Injectable]
+    [MultipleInjectable]
     public interface IEmisionEndpoint
     {
         void Write(string location, string body);
@@ -15,6 +19,14 @@ namespace TypeSharpGenLauncher.Core.Synthesiser
         {
             Console.WriteLine($"Writing to {location}:");
             Console.Write(body);
+        }
+    }
+
+    public class FileEndpoint : IEmisionEndpoint
+    {
+        public void Write(string location, string body)
+        {
+            File.CreateNew(location, Encoding.ASCII.GetBytes(body));
         }
     }
 }
