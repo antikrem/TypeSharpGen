d52c0ddbc30fe6b36621f70e580696e580090c6f
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-12-14 01:53:56 +1000

    Fix

--- TypeSharpGen/Models/ITypeDefinition.cs
+++ TypeSharpGen/Models/ITypeDefinition.cs
@@ -4,6 +4,18 @@ using System.Collections.Generic;
 
 namespace TypeSharpGen.Builder
 {
+    public enum Symbol
+    {
+        Class,
+        Interface
+    }
+
+    public static class SymbolExtensions
+    {
+        static public string ToText(this Symbol symbol) 
+            => Enum.GetName(symbol)!.ToLower();
+    }
+
     public interface ITypeDefinition
     {
         Type Type { get; }
