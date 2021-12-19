c213003831667e79e738f548844710b8b1643741
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-12-19 12:59:49 +1000

    Moved modifier to another file

---
+++ TypeSharpGen/Models/MethodModifier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace TypeSharpGen.Builder
+{
+    public interface IMethodModifier
+    {
+        MethodDefinition Apply(MethodDefinition method);
+    }
+
+    public class OverideReturnType : IMethodModifier
+    {
+        private Type _type;
+
+        public OverideReturnType(Type type)
+        {
+            _type = type;
+        }
+
+        public MethodDefinition Apply(MethodDefinition method)
+        {
+            method.OverrideReturnType(_type);
+            return method;
+        }
+    }
+}
--- TypeSharpGen/Models/TypeBuilder.cs
+++ TypeSharpGen/Models/TypeBuilder.cs
@@ -8,27 +8,6 @@ using EphemeralEx.Extensions;
 
 namespace TypeSharpGen.Builder
 {
-    public interface IMethodModifier
-    {
-        MethodDefinition Apply(MethodDefinition method);
-    }
-
-    public class OverideReturnType : IMethodModifier
-    {
-        private Type _type;
-
-        public OverideReturnType(Type type)
-        {
-            _type = type;
-        }
-
-        public MethodDefinition Apply(MethodDefinition method)
-        {
-            method.OverrideReturnType(_type);
-            return method;
-        }
-    }
-
     public class OverideReturnType<T> : IMethodModifier
     {
         public MethodDefinition Apply(MethodDefinition method)
