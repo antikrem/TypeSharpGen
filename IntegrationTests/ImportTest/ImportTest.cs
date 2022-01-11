397a83478055e488406337a8fbecc6d5ea621aee
Chalinda Rodrigo <nobleservant49@gmail.com>
2022-01-11 22:48:09 +1000

    Added import test

---
+++ IntegrationTests/ImportTest/ImportTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+using TypeSharpGen.Builder;
+
+
+namespace IntegrationTests.Import
+{
+    public class ImportTestDependencyClass
+    {
+        public int SimpleProperty { get; }
+    }
+
+    public class ImportTestConsumerClass
+    {
+        public ImportTestDependencyClass DependenyProperty { get; }
+    }
+
+    public class ImportTest : IntegrationTest
+    {
+        public override IEnumerable<ITypeDefinition> TypeDeclaractions()
+        {
+            yield return Declare(typeof(ImportTestDependencyClass))
+                .AddProperty("SimpleProperty")
+                .EmitTo("ImportTestDependencyClass.ts");
+
+            yield return Declare(typeof(ImportTestConsumerClass))
+                .AddProperty("DependenyProperty")
+                .EmitTo("ImportTestConsumerClass.ts");
+        }
+    }
+}
