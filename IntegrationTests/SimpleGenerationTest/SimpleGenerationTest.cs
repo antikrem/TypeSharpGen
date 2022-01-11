eddcaed8f6a7ef9a5c87a0998369b1e04f1624b3
Chalinda Rodrigo <nobleservant49@gmail.com>
2022-01-11 20:24:55 +1000

    Changed test to include method

---
+++ IntegrationTests/SimpleGenerationTest/SimpleGenerationTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+using TypeSharpGen.Builder;
+
+
+namespace IntegrationTests.PropertiesTest
+{
+    public class SimpleGenerationTestClass
+    {
+        public int SimpleProperty { get; }
+
+        public int SimpleMethod(float value)
+        {
+            return (int)value;
+        }
+    }
+
+    public class SimpleGenerationTest : IntegrationTest
+    {
+        public override IEnumerable<ITypeDefinition> TypeDeclaractions()
+        {
+            yield return Declare(typeof(SimpleGenerationTestClass))
+                .AddProperty("SimpleProperty")
+                .AddMethod("SimpleMethod")
+                .EmitTo("SimpleGenerationTest.ts");
+        }
+    }
+}
