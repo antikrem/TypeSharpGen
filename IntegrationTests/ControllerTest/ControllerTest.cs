0b0162e6562b29cfc4e113353c34f0ad5f9ba8f9
Chalinda Rodrigo <nobleservant49@gmail.com>
2022-01-12 00:17:24 +1000

    Added controller integration test

---
+++ IntegrationTests/ControllerTest/ControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TypeSharpGen.Builder;
+
+
+namespace IntegrationTests.Controller
+{
+    public class Dependency
+    {
+        public int SimpleProperty { get; }
+    }
+
+    public class ViewModel
+    {
+        public Dependency Property { get; }
+    }
+
+    public class Controller
+    {
+        public ViewModel Action(int parameter) => new ViewModel();
+    }
+
+    public class ControllerTest : IntegrationTest
+    {
+        public override IEnumerable<ITypeDefinition> TypeDeclaractions()
+        {
+            yield return Declare(typeof(ViewModel))
+                .AddProperty("Property")
+                .EmitTo("ViewModel.ts");
+
+            yield return Declare(typeof(Controller))
+                .AddMethod("Action", new OverideReturnType<Task<ViewModel>>())
+                .EmitTo("Controller.ts");
+        }
+    }
+}
